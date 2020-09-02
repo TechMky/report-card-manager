9bc5829458d664766f5583f375fdd5e7b9757277
Yashasvi Sinha <yashasvi.kumar.sinha@gmail.com>
2020-09-02 22:09:15 +0530

    :sparkles: added report card addition

--- main_menu.py
+++ main_menu.py
@@ -1,3 +1,6 @@
+from models import ReportCard, ReportCardSubject
+from db import Session
+
 def get_main_menu():
     menu = '''
     1. Add New Report Card
@@ -16,4 +19,45 @@ def get_handlers():
     }
 
 def add_report_card():
-    pass
+    report_card_dict = {
+        "student_name" : input("Enter Student's Name: "),
+        "student_addmission_number": input("Enter Student's Admission Number: "),
+        "student_class": input("Enter Student's class: "),
+        "student_section": input("Enter Student's section: "),
+    }
+
+    no_of_subject = int(input("How many subjects do you want to add ? "))
+    
+    if no_of_subject < 1:
+        print("Report Card cannot be added with no subjects")
+        return
+
+    # get subject's data
+    report_card_dict["report_card_subjects"] = get_report_card_subjects( no_of_subject )
+
+    report_card = ReportCard(**report_card_dict)
+
+    session = Session()
+    session.add(report_card)
+    session.commit()
+
+    print("Report Card Added!!")
+
+
+def get_report_card_subjects(subject_count: int):
+    
+    report_card_subject_list = []
+    
+    for subject_number in range(subject_count):
+        
+        report_card_subject_dict = {
+            "subject_name": input(f"Enter Subject { subject_number + 1 }'s Name: "),
+            "marks": float(input(f"Enter Subject { subject_number + 1 }'s marks obtained: ")),
+            "max_marks": float(input(f"Enter Subject { subject_number + 1 }'s maximum marks: ")),
+        }
+
+        report_card_subject = ReportCardSubject(**report_card_subject_dict)
+
+        report_card_subject_list.append(report_card_subject)
+    
+    return report_card_subject_list
