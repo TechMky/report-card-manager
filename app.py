ea07f9796025958efbea75613280fac16a658237
Yashasvi Sinha <yashasvi.kumar.sinha@gmail.com>
2020-09-02 21:40:27 +0530

    :hammer: refactored db connection and initialization

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+from db import init_db
 from helper import get_choice_from_menu
 from main_menu import get_main_menu
 
+
+# setup db/connection
+init_db()
 
 #main menu
 main_menu, main_menu_handlers = get_main_menu()
--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import logging
+from models import ReportCard, ReportCardSubject
 from config import DATABASE_URL
 from sqlalchemy import create_engine
 from sqlalchemy import create_engine
@@ -7,19 +8,21 @@ from sqlalchemy.orm import sessionmaker, with_expression
 
 LOG_FORMAT = "%(asctime)s %(name)s %(levelname)s %(message)s"
 
-handler = logging.FileHandler('sql.log')
-handler.setFormatter(logging.Formatter(LOG_FORMAT))
-
-sql_logger = logging.getLogger('sqlalchemy.engine')
-sql_logger.setLevel(logging.DEBUG)
-sql_logger.addHandler(handler)
 
 engine = create_engine(DATABASE_URL)
 
-with engine.connect() as conn:
-    rs = conn.execute('SHOW DATABASES;')
-    for row in rs:
-        print(row)
 
 Session = sessionmaker(bind=engine)
 
+def init_db():
+    # setup db logging
+    handler = logging.FileHandler('sql.log')
+    handler.setFormatter(logging.Formatter(LOG_FORMAT))
+
+    sql_logger = logging.getLogger('sqlalchemy.engine')
+    sql_logger.setLevel(logging.DEBUG)
+    sql_logger.addHandler(handler)
+
+    #initialize tables in the database using the models
+    ReportCard.__table__.create(bind=engine, checkfirst=True)
+    ReportCardSubject.__table__.create(bind=engine, checkfirst=True)
