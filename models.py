c76670e5f1f2c91a68aed26caf136107e20e15cf
Yashasvi Sinha <yashasvi.kumar.sinha@gmail.com>
2020-09-02 21:39:32 +0530

    :sparkles: added report card and report subject model

---
+++ models.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.declarative.api import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Column, ForeignKey, Sequence
+from sqlalchemy.sql.sqltypes import Float, Integer, String
+
+
+Base = declarative_base()
+
+class ReportCard(Base):
+    __tablename__ = 'report_card'
+    report_card_id = Column(Integer, Sequence('report_card_id'), primary_key=True)
+    student_name = Column(String(255))
+    student_addmission_number = Column(String(255))
+    student_class = Column(String(50))
+    student_section = Column(String(10))
+    report_card_subjects = relationship('ReportCardSubject', backref='report_card', lazy=True, cascade='all, delete-orphan')
+
+
+class ReportCardSubject(Base):
+    __tablename__ = 'report_card_subject'
+    report_card_subject_id = Column(Integer, Sequence('report_card_subject_id'), primary_key=True)
+    report_card_id = Column(Integer, ForeignKey('report_card.report_card_id'))
+    subject_name = Column(String(255))
+    marks = Column(Float)
+    max_marks = Column(Float)
--- models/ReportCard.py
+++
@@ -1,2 +0,0 @@
-class ReportCard(object):
-    pass
