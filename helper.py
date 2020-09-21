ba0bd22b0d5544f13fb84584a5fa37b7094b2a0d
Yashasvi Sinha <yashasvi.kumar.sinha@gmail.com>
2020-09-21 16:54:16 +0530

    moved table print to helper

--- helper.py
+++ helper.py
@@ -1,3 +1,6 @@
+from typing import List
+from rich.console import Console
+from rich.table import Table
 from models import ReportCard
 # helper functions
 
@@ -34,3 +37,35 @@ def get_query_from_student_search_params(search_params: list):
             queryList.append(ReportCard.student_addmission_number == param)
 
     return queryList
+
+def pretty_print_report_card(result: List[ReportCard] ):
+
+    console = Console()
+    # extract this to a helper function afterwards
+    table = Table(show_header=True, header_style="bold green")
+    table.add_column("ID")
+    table.add_column("Student Name")
+    table.add_column("Admission Number")
+    table.add_column("Class - Section")
+    table.add_column("Percentage")
+    
+    for report_card in result:
+
+        # can optimize to use a single loop
+        max_total_marks = sum([ subject.max_marks for subject in report_card.report_card_subjects ])
+        obtained_marks = sum([ subject.marks for subject in report_card.report_card_subjects ])
+
+        percentage = (obtained_marks / max_total_marks) * 100
+        percentage = str(round(percentage, 2)) + ' %'
+
+        table.add_row(
+
+            str(report_card.report_card_id), 
+            report_card.student_name, 
+            report_card.student_addmission_number, 
+            f"{report_card.student_class} - {report_card.student_section}", 
+            percentage
+
+        )
+
+    console.print(table)
